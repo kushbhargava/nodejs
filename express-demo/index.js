cdeaa66c91a6990bac35d379855c61f02e959340
Kushagra Bhargava <kushagra.bhargava101@gmail.com>
2018-09-14 10:15:48 +0530

    added for rest with express

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,26 +1,72 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
 
-const courses = [
-    { id: 1, name: 'Course1' },
-    { id: 2, name: 'Course1' },
+app.use(express.json());
+
+const products = [
+    { id: 1, name: 'Product 1' },
+    { id: 2, name: 'Product 2' },
+    { id: 3, name: 'Product 3' },
 ];
+
 app.get('/', (req, res) => {
-    res.send("Hello world!!!");
+    res.send('Inside Api!!');
 });
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
+app.get('/api/products', (req, res) => {
+    res.send(products);
 });
 
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p.id === parseInt(req.params.id));
 
-    if (!course) res.status(404).send('course could not be found');
+    if (!product) res.status(404).send('Product for the given id is not available!');
 
-    res.send(course);
+    res.send(product);
 });
+
+app.post('/api/products', (req, res) => {
+    const { error } = validatProduct(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const product = {
+        id: products.length + 1,
+        name: req.body.name
+    };
+    products.push(product);
+    res.send(product);
+});
+
+app.put('/api/products/:id', (req, res) => {
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) return res.status(404).send('Product cant be found!');
+
+    const { error } = validatProduct(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    product.name = req.body.name;
+    res.send(product);
+});
+
+app.delete('/api/products/:id', (req, res) => {
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) return res.status(404).send('Product cant be found!');
+
+    const index = products.indexOf(product);
+    products.splice(index, 1);
+
+    res.send(product);
+});
+
+
+function validatProduct(product) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(product, schema);
+}
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening to port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
